4bab9049eab6ffc7fb475f743303fb6f6d100d9e
Nikolai Schnell <nikolaischnell@gmx.de>
2021-07-11 10:06:39 +0200

    remove logging of PFCs, ground all rigid preconditions

--- src/algo/fact_analysis.cpp
+++ src/algo/fact_analysis.cpp
@@ -303,6 +303,15 @@ SigSet FactAnalysis::getPossibleFactChanges(const USignature& sig) {
             if (_htn.isFullyGround(prerequisite._usig) && !_htn.hasQConstants(prerequisite._usig)) {
                 //Log::d("Found rigid predicate: %s\n", TOSTR(prerequisite));
                 reachable = !prerequisite._negated != !_init_state.count(prerequisite._usig);
+            } else {
+                reachable = false;
+                for (const auto& groundFact : ArgIterator::getFullInstantiationQConst(prerequisite._usig, _htn)) {
+                    //Log::d("Ground fact: %s\n", TOSTR(groundFact));
+                    if (_init_state.count(groundFact) == !prerequisite._negated) {
+                        reachable = true;
+                        break;
+                    }
+                }
             }
             if (!reachable) {
                 //Log::d("Found impossible rigid prereq: %s\n", TOSTR(prerequisite));
