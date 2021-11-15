e66607441f556dc867042d20304027ca8df46abd
Nikolai Schnell <nikolaischnell@gmx.de>
2021-11-15 11:42:40 +0100

    add panda scripts

--- execute_pandagbfs.sh
+++ execute_pandagbfs.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
-TEMP_DIR = $1
-PROBLEM_NUMBER = $2
-DOMAIN_DIR = $3
-INSTANCE_DIR = $4
-PARSED_FILE = ${TEMP_DIR}/${PROBLEM_NUMBER}.parsed
-SAS_FILE = ${TEMP_DIR}/${PROBLEM_NUMBER}.sas
-./home/schnell/lilotane/pandaPIparser $DOMAIN_DIR $INSTANCE_DIR $PARSED_FILE
-./home/schnell/lilotane/pandaPIgrounder $PARSED_FILE $SAS_FILE
-./home/schnell/lilotane/pandaPIengine --heuristic='rc2(add)' $SAS_FILE
+TEMP_DIR=$1
+PROBLEM_NUMBER=$2
+DOMAIN_DIR=$3
+INSTANCE_DIR=$4
+BINARIES_DIR=$5
+PARSED_FILE=${TEMP_DIR}/${PROBLEM_NUMBER}.parsed
+SAS_FILE=${TEMP_DIR}/${PROBLEM_NUMBER}.sas
+_term() { 
+  echo "Caught SIGTERM signal!" 
+  kill -TERM "$child" 2>/dev/null
+}
+trap _term SIGINT
+trap _term SIGKILL
+${BINARIES_DIR}/pandaPIparser $DOMAIN_DIR $INSTANCE_DIR $PARSED_FILE
+${BINARIES_DIR}/pandaPIgrounder $PARSED_FILE $SAS_FILE &
+child=$!
+
+wait "$child"
+
+${BINARIES_DIR}/pandaPIengine --heuristic='rc2(add)' $SAS_FILE &
+
+child=$!
+wait "$child"
+
+rm $PARSED_FILE
+rm $SAS_FILE
--- execute_pandasat.sh
+++ execute_pandasat.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
-TEMP_DIR = $1
-PROBLEM_NUMBER = $2
-DOMAIN_DIR = $3
-INSTANCE_DIR = $4
-CRYPTOMINISAT_LIB = $5
-PARSED_FILE = ${TEMP_DIR}/${PROBLEM_NUMBER}.parsed
-SAS_FILE = ${TEMP_DIR}/${PROBLEM_NUMBER}.sas
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CRYPTOMINISAT_LIB
-./home/schnell/lilotane/pandaPIparser $DOMAIN_DIR $INSTANCE_DIR $PARSED_FILE
-./home/schnell/lilotane/pandaPIgrounder $PARSED_FILE $SAS_FILE
-./home/schnell/lilotane/pandaPIengine -s $SAS_FILE
+TEMP_DIR=$1
+PROBLEM_NUMBER=$2
+DOMAIN_DIR=$3
+INSTANCE_DIR=$4
+BINARIES_DIR=$5
+PARSED_FILE=${TEMP_DIR}/${PROBLEM_NUMBER}.parsed
+SAS_FILE=${TEMP_DIR}/${PROBLEM_NUMBER}.sas
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$BINARIES_DIR
+
+_term() {
+  echo "Caught SIGTERM signal!" 
+  kill -TERM "$child" 2>/dev/null
+}
+trap _term SIGINT
+trap _term SIGKILL
+${BINARIES_DIR}/pandaPIparser $DOMAIN_DIR $INSTANCE_DIR $PARSED_FILE
+${BINARIES_DIR}/pandaPIgrounder $PARSED_FILE $SAS_FILE &
+child=$!
+
+wait "$child"
+
+${BINARIES_DIR}/pandaPIengineSAT -s $SAS_FILE &
+
+child=$!
+wait "$child"
+
+rm $SAS_FILE
+rm $PARSED_FILE
